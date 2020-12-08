9f4b0a9528ad48e2be36872bc5968a1ac1412b24
AsterNighT <klxjt99@outlook.com>
2020-12-08 18:41:54 +0800

    update ignore

--- 2018_topic2_student/fuseRelaunch.cmd
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/ISE/2018_topic2_student/2018_topic2_student/sim_mips_isim_beh.exe" -prj "D:/ISE/2018_topic2_student/2018_topic2_student/sim_mips_beh.prj" "work.sim_mips" "work.glbl" 
-=======
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "F:/Meyas/computer_architecture_lab/2018_topic2_student/sim_mips_isim_beh.exe" -prj "F:/Meyas/computer_architecture_lab/2018_topic2_student/sim_mips_beh.prj" "work.sim_mips" "work.glbl" 
->>>>>>> 4e083c137bc8150fcb2cec47a8514ca0fd75706e
--- README.md
+++
@@ -1,4 +0,0 @@
-# computer_architecture_lab
-lab of ca course
-
-lab1：验收正确 仿真待完成
